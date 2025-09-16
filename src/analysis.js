0db6856736b3d52a6d770acb98ddf1de991f824c
Aiden Lee <aidenlee@sas.upenn.edu>
2025-09-16 13:51:21 -0400

    add implementation for analysis and main

--- src/analysis.js
+++ src/analysis.js
@@ -1,6 +1,8 @@
 /**
  * [TODO] Step 0: Import the dependencies, fs and papaparse
  */
+const fs = require('fs');
+const Papa = require('papaparse');
 
 /**
  * [TODO] Step 1: Parse the Data
@@ -9,7 +11,11 @@
  * @param {string} filename - path to the csv file to be parsed
  * @returns {Object} - The parsed csv file of app reviews from papaparse.
  */
-function parseData(filename) {}
+function parseData(filename) {
+    const data = fs.readFileSync(filename, 'utf8');
+    const csv = Papa.parse(data, { header: true });
+    return csv;
+}
 
 /**
  * [TODO] Step 2: Clean the Data
@@ -26,7 +32,32 @@ function parseData(filename) {}
  * @param {Object} csv - a parsed csv file of app reviews
  * @returns {Object} - a cleaned csv file with proper data types and removed null values
  */
-function cleanData(csv) {}
+function cleanData(csv) {
+    const data = csv.data.filter(
+        (row) =>
+            !Object.entries(row).some(
+                ([k, v]) => k !== 'user_gender' && v === '',
+            ),
+    );
+
+    data.forEach((row) => {
+        row['review_id'] = parseInt(row['review_id']);
+        row['user_id'] = parseInt(row['user_id']);
+        row['num_helpful_votes'] = parseInt(row['num_helpful_votes']);
+        row['user_age'] = parseInt(row['user_age']);
+        row['rating'] = parseFloat(row['rating']);
+        row['review_data'] = Date.parse(row['review_data']);
+        row['verified_purchase'] = row['verified_purchase'] === 'true';
+        row['user'] = {
+            user_id: row['user_id'],
+            user_age: row['user_age'],
+            user_country: row['user_country'],
+            user_gender: row['user_gender'],
+        };
+    });
+    // console.log(data);
+    return data;
+}
 
 /**
  * [TODO] Step 3: Sentiment Analysis
@@ -38,7 +69,10 @@ function cleanData(csv) {}
  * @returns {string} - 'positive' if rating is greater than 4, negative is rating is below 2,
  *                      and neutral if it is between 2 and 4.
  */
-function labelSentiment({ rating }) {}
+function labelSentiment({ rating }) {
+    const res = rating > 4 ? 'positive' : rating >= 2 ? 'neutral' : 'negative';
+    return res;
+}
 
 /**
  * [TODO] Step 3: Sentiment Analysis by App
@@ -48,7 +82,23 @@ function labelSentiment({ rating }) {}
  * @param {Object} cleaned - the cleaned csv data
  * @returns {{app_name: string, positive: number, neutral: number, negative: number}[]} - An array of objects, each summarizing sentiment counts for an app
  */
-function sentimentAnalysisApp(cleaned) {}
+function sentimentAnalysisApp(cleaned) {
+    const res = {};
+
+    for (const obj of cleaned) {
+        if (!(obj.app_name in res)) {
+            res[obj.app_name] = {
+                app_name: obj.app_name,
+                positive: 0,
+                neutral: 0,
+                negative: 0,
+            };
+        }
+        res[obj.app_name][labelSentiment({ rating: obj.rating })] += 1;
+    }
+    // console.log(res);
+    return Object.values(res);
+}
 
 /**
  * [TODO] Step 3: Sentiment Analysis by Language
@@ -58,7 +108,23 @@ function sentimentAnalysisApp(cleaned) {}
  * @param {Object} cleaned - the cleaned csv data
  * @returns {{lang_name: string, positive: number, neutral: number, negative: number}[]} - An array of objects, each summarizing sentiment counts for a language
  */
-function sentimentAnalysisLang(cleaned) {}
+function sentimentAnalysisLang(cleaned) {
+    const res = {};
+
+    for (const obj of cleaned) {
+        if (!(obj.review_language in res)) {
+            res[obj.review_language] = {
+                lang_name: obj.review_language,
+                positive: 0,
+                neutral: 0,
+                negative: 0,
+            };
+        }
+        res[obj.review_language][labelSentiment({ rating: obj.rating })] += 1;
+    }
+    // console.log(res);
+    return Object.values(res);
+}
 
 /**
  * [TODO] Step 4: Statistical Analysis
@@ -75,7 +141,44 @@ function sentimentAnalysisLang(cleaned) {}
  * @returns {{mostReviewedApp: string, mostReviews: number, mostUsedDevice: String, mostDevices: number, avgRating: float}} -
  *          the object containing the answers to the desired summary statistics, in this specific format.
  */
-function summaryStatistics(cleaned) {}
+function summaryStatistics(cleaned) {
+    const app_sentiments = sentimentAnalysisApp(cleaned);
+    app_sentiments.forEach((row) => {
+        row['sum'] = row['positive'] + row['neutral'] + row['negative'];
+    });
+    const most_reviewed_app = app_sentiments.reduce((max, curr) => {
+        return max['sum'] < curr['sum'] ? curr : max;
+    }, app_sentiments[0]);
+    // console.log(most_reviewed_app);
+
+    // let cnt = 0;
+    let sum = 0;
+    const hashmap = {};
+    for (const row of cleaned) {
+        if (row.app_name === most_reviewed_app.app_name) {
+            // cnt += 1;
+            sum += row.rating;
+            if (!(row.device_type in hashmap)) {
+                hashmap[row.device_type] = 0;
+            }
+            hashmap[row.device_type] += 1;
+        }
+    }
+
+    // console.log(hashmap);
+
+    const device = Object.keys(hashmap).reduce((max, curr) => {
+        return hashmap[max] < hashmap[curr] ? curr : max;
+    });
+
+    return {
+        mostReviewedApp: most_reviewed_app.app_name,
+        mostReviews: most_reviewed_app.sum,
+        mostUsedDevice: device,
+        mostDevices: hashmap[device],
+        avgRating: sum / most_reviewed_app.sum,
+    };
+}
 
 /**
  * Do NOT modify this section!
@@ -86,5 +189,5 @@ module.exports = {
     sentimentAnalysisApp,
     sentimentAnalysisLang,
     summaryStatistics,
-    labelSentiment
+    labelSentiment,
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /**
- * You may use this file to call the functions within your code for testing purposes. 
+ * You may use this file to call the functions within your code for testing purposes.
  * Code written in this file will not be graded or submitted.
  * The steps are labeled for your convenience.
  */
@@ -16,18 +16,26 @@ const {
  *      From the analysis.js file, call the parseData method with the correct file path to the data file.
  */
 
+const data = parseData('src/multilingual_mobile_app_reviews_2025.csv');
 /**
  * Step 2: Call the cleanData function
  *      Pass the csv as an argument to the cleanData function.
  */
+const cleaned_data = cleanData(data);
 
 /**
  * Step 3: Sentiment Analysis
  *      Call the printSentimentAnalysis function get a summary
  *      of the sentiments of apps across different apps and languages.
  */
+const sentiments_app = sentimentAnalysisApp(cleaned_data);
+const sentiments_lang = sentimentAnalysisLang(cleaned_data);
+console.log('Sentiment by app: ', sentiments_app);
+console.log('Sentiment by lang: ', sentiments_lang);
 
 /**
  * Step 4: Statistical Analysis
  *      Call the printAnalysis function to get some summary statistics of the cleaned data.
  */
+
+console.log(summaryStatistics(cleaned_data));
